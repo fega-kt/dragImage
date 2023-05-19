c0a5ef8bc8107a5768fe93ecf70a6c39c69baf66
fe gà <fega.kt@gmail.com>
2023-05-20 05:22:52 +0700

    update validate drag item no has image

--- js/script.js
+++ js/script.js
@@ -2,18 +2,26 @@
 const listItem = document.querySelectorAll(".item");
 const image = document.querySelector(".imageDrag");
 console.log(listItem)
+let beforItem, afterItem
 listItem.forEach(item => {
-    item.addEventListener("dragover", (e)=>{
+    item.addEventListener("dragover", (e) => {
+        if (!beforItem) return
         e.preventDefault();
         item.classList.add('hovered')
     })
-
-    item.addEventListener("dragleave", (e)=>{
+    item.addEventListener("drag", (e) => {
+        beforItem = item.querySelector(".imageDrag");
         e.preventDefault();
+
+    })
+    item.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        if (!beforItem) return
         item.classList.remove('hovered')
     })
 
-    item.addEventListener("drop", (e)=>{
+    item.addEventListener("drop", (e) => {
+        if (!beforItem) return
         e.preventDefault();
         item.append(image)
         item.classList.remove('hovered')
